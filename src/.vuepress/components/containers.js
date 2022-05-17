d1a18434a01434826b6463c03697712fd9dbb4b6
Yakovlev Kll <yakovlev.kll@gmail.com>
2022-05-17 14:24:11 +0000

    Containers update

--- src/.vuepress/components/containers.js
+++ src/.vuepress/components/containers.js
@@ -1,49 +1,134 @@
 module.exports = [
-    [
-        "vuepress-plugin-container",
-        {
-          type: "example",
-          before: info => `<div class="block example-block">
-          <div class="block-title example-title">Пример ${info}</div>
-          <div class="block-contents">`,
-          after: "</div></div>"
+  [
+    "vuepress-plugin-container",
+    {
+      type: "example",
+      before: `<div class="block example">
+          <div class="title">Пример</div>`,
+      after: "</div>"
+    }
+  ],
+  [
+    "vuepress-plugin-container",
+    {
+      type: "hw",
+      before: `<div class="block hw">
+          <div class="title">Homework</div>`,
+      after: "</div>"
+    }
+  ],
+  [
+    "vuepress-plugin-container",
+    {
+      type: "nb",
+      before: (info) => `<div class="block nb">
+          <div class="title">Важно</div>`,
+      after: "</div>"
+    }
+  ],
+  [
+    "vuepress-plugin-container",
+    {
+      type: "footnote",
+      before: `<div class="block footnote">
+          <div class="title">Примечание</div>`,
+      after: "</div>"
+    }
+  ],
+  [
+    "vuepress-plugin-container",
+    {
+      type: "quote",
+      before: `<div class="quote block">`,
+      after: (info) =>
+        `<div class="author">${info}</div>
+            </div>`
+    }
+  ],
+  [
+    "vuepress-plugin-container",
+    {
+      type: "quiz",
+      before: `<div class="block quiz">
+          <div class="title">Викторина</div>`,
+      after: "</div>"
+    }
+  ],
+  [
+    "vuepress-plugin-container",
+    {
+      type: "principle",
+      before: (info) => `<div class="principle block">
+          <div class="title">${info}</div>`,
+      after: `</div>`
+    }
+  ],
+  [
+    "vuepress-plugin-container",
+    {
+      type: "tasks",
+      before: `<div class="block tasks">
+          <div class="title">Задания</div>`,
+      after: "</div>"
+    }
+  ],
+  [
+    "vuepress-plugin-container",
+    {
+      type: "todo",
+      before: (info) => {
+        let res = "";
+
+        if (info.length > 2) {
+          switch (info) {
+            case "table":
+              res = "таблицу";
+              break;
+            case "img":
+              res = "картинку";
+              break;
+            case "task":
+              res = "задание";
+              break;
+            default:
+              res = "";
+              break;
+          }
+          res = `<div class="title">Добавить ${res}</div>`;
         }
-      ],
-      [
-        "vuepress-plugin-container",
-        {
-          type: "task",
-          before: info => `<div class="block task-block">
-          <div class="block-title task-title">Задание ${info}</div>
-          <div class="block-contents">`,
-          after: "</div></div>"
-        }
-      ],
-      [
-        "vuepress-plugin-container",
-        {
-          type: "nb",
-          before: info => `<div class="block nb-block">
-          <div class="block-title nb-title">Важно ${info}</div>
-          <div class="block-contents">`,
-          after: "</div></div>"
-        }
-      ],
-      [
-        "vuepress-plugin-container",
-        {
-          type: "quest",
-          before: info => `<div class="block quest-block">
-          <div class="block-title quest-title">Вопрос ${info}</div>
-          <div class="block-contents">`,
-          after: "</div></div>"
-        }
-      ],
-      [
-        "vuepress-plugin-container",
-        {
-          type: "todo",
-          defaultTitle: ""
-        }
-      ],
+
+        return `<div class="block todo">${res}`;
+      },
+      after: "</div>"
+    }
+  ],
+
+  [
+    "vuepress-plugin-container",
+    {
+      type: "wow",
+      before: (title) => `<div class="block wow">
+          <div class="title">${title}</div>`,
+      after: "</div>"
+    }
+  ],
+  [
+    "vuepress-plugin-container",
+    {
+      type: "youtube",
+      before: (src) =>
+        `<iframe class="youtube-frame" src="https://www.youtube.com/embed/${src}" frameborder="0" allowfullscreen>`,
+      after: "</iframe>"
+    }
+  ],
+  [
+    "vuepress-plugin-container",
+    {
+      type: "desmos",
+      before: (src) => `<iframe class="desmos-frame"
+      src="https://www.desmos.com/calculator/${src}"
+      frameborder="0">`,
+      after: "</iframe>"
+    }
+  ]
 ]
