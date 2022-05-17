afd1d5022f2190d0a0b3bdac3232fdd451941c74
Yakovlev Kll <yakovlev.kll@gmail.com>
2022-05-17 12:45:22 +0000

    Config updated

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,8 +2,12 @@ const { description } = require('../../package')
 const containers = require("./components/containers.js");
 const sidebarEn = require("./sidebars/en.json");
 const sidebarRu = require("./sidebars/ru.json");
+// const katex = require('@iktakahiro/markdown-it-katex')
+import katex from '@iktakahiro/markdown-it-katex'
 
-module.exports = {
+import {defineConfig} from 'vuepress/config'
+
+export default defineConfig({
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
@@ -21,7 +25,19 @@ module.exports = {
   head: [
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+
+    ['link', { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/katex@0.15.3/dist/katex.min.css' }],
+
+    [
+      "link",
+      {
+        rel: "stylesheet",
+        href:
+          "https://cdn.jsdelivr.net/github-markdown-css/2.2.1/github-markdown.css"
+      }
+    ],
+
   ],
 
   /**
@@ -68,54 +84,16 @@ module.exports = {
    * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
    */
   plugins: [
-    [
-      "vuepress-plugin-container",
-      {
-        type: "example",
-        before: info => `<div class="block example-block">
-        <div class="block-title example-title">Пример ${info}</div>
-        <div class="block-contents">`,
-        after: "</div></div>"
-      }
-    ],
-    [
-      "vuepress-plugin-container",
-      {
-        type: "task",
-        before: info => `<div class="block task-block">
-        <div class="block-title task-title">Задание ${info}</div>
-        <div class="block-contents">`,
-        after: "</div></div>"
-      }
-    ],
-    [
-      "vuepress-plugin-container",
-      {
-        type: "nb",
-        before: info => `<div class="block nb-block">
-        <div class="block-title nb-title">Важно ${info}</div>
-        <div class="block-contents">`,
-        after: "</div></div>"
-      }
-    ],
-    [
-      "vuepress-plugin-container",
-      {
-        type: "quest",
-        before: info => `<div class="block quest-block">
-        <div class="block-title quest-title">Вопрос ${info}</div>
-        <div class="block-contents">`,
-        after: "</div></div>"
-      }
-    ],
-    [
-      "vuepress-plugin-container",
-      {
-        type: "todo",
-        defaultTitle: ""
-      }
-    ],
+    ...containers,
     '@vuepress/plugin-back-to-top',
     '@vuepress/plugin-medium-zoom',
-  ]
-}
+  ],
+
+  markdown: {
+    lineNumbers: true,
+    extendMarkdown: md => {
+      md.set({ typographer: true });
+      md.use(katex)
+    }
+  },
+})
